7d60818eb2a379ec62912a102bfd185304db3240
KarUladz <u.karnialiuk@gmail.com>
2025-07-06 13:05:58 +0300

    feat: add m4-task4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int res = 0;
+        if (first > second ) {
+            res = first;
+        }
+        else if (second > first) {
+            res = second;
+        }
+        res = third > res ? third : res;
+        System.out.println(res);
     }
 }
